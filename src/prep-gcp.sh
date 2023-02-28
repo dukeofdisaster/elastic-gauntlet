10bf75094c18380f28455d05a616efbf3e346b7b
boxuser <boxuser@localbox.local>
2023-02-28 12:32:50 -0700

    add src scripts

--- src/prep-gcp.sh
+++ src/prep-gcp.sh
@@ -6,11 +6,29 @@
 #   1. apt install socat
 #   1. stop elasticsaerch service
 #   1. set the security bools 
+echo "This script uses apt package manager to install socat and edits elasticsearch config like so"
+echo "============================================================================================"
+echo "
+
 apt update 
 apt install socat
 service elasticsearch stop
 echo -ne c29jYXQgVENQLUxJU1RFTjo5MjIyLGZvcmsgVENQOjEyNy4wLjAuMTo5MjAwICY= |base64 -d > /tmp/poormans-proxy.sh
-echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
-echo "xpack.security.authc.api_key.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo \"xpack.security.enabled: true\" >> /etc/elasticsearch/elasticsearch.yml
+echo \"xpack.security.authc.api_key.enabled: true\" >> /etc/elasticsearch/elasticsearch.yml
 /usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto
 bash /tmp/poormans-proxy.sh
+"
+echo "============================================================================================"
+echo "Do you want to run the above? [y/n]"
+read resp
+if [[ "$resp" == "y" || "$resp" ==  "Y" ]];then
+    apt update 
+    apt install socat
+    service elasticsearch stop
+    echo -ne c29jYXQgVENQLUxJU1RFTjo5MjIyLGZvcmsgVENQOjEyNy4wLjAuMTo5MjAwICY= |base64 -d > /tmp/poormans-proxy.sh
+    echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+    echo "xpack.security.authc.api_key.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+    /usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto
+    bash /tmp/poormans-proxy.sh
+fi
